1832843da5b39f479bdd55e8fc8b37571c6619bb
dihia <dihiasalhi@gmail.com>
2025-12-10 14:57:28 -0500

    feat: first implementation of a working unit test using 'assertEquals'

--- test/MyMathTest.java
+++ test/MyMathTest.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Test;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMathTest {
 
     @Test
     void calculateSum() {
-     //   fail("not implemented yet");
+        int [] numbers = {1,2,3};
+     MyMath myMath = new MyMath();
+     int result = myMath.calculateSum(numbers);
+        System.out.println(result);
+        int expectedRrsult = 6;
+        assertEquals(expectedRrsult, result);
     }
 }
