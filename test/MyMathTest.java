337a95ed68b3d0b7890a2f0e2345ab614d7680c7
dihia <dihiasalhi@gmail.com>
2025-12-10 15:48:15 -0500

    First code and First Unit Test

--- test/MyMathTest.java
+++ test/MyMathTest.java
@@ -5,14 +5,14 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMathTest {
+    MyMath myMath = new MyMath();
+    @Test
+    void calculateSum_EmptyArray() {
+        assertEquals(0, myMath.calculateSum(new int[]{}));
+    }
 
     @Test
-    void calculateSum() {
-        int [] numbers = {1,2,3};
-     MyMath myMath = new MyMath();
-     int result = myMath.calculateSum(numbers);
-        System.out.println(result);
-        int expectedRrsult = 6;
-        assertEquals(expectedRrsult, result);
+    void calculateSum_ThreeMumberArray() {
+        assertEquals(6, myMath.calculateSum(new int[]{1,2,3}));
     }
 }
